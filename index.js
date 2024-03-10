3270642f016487c60aa7ea2497e40f3a84e58703
Robert Solorzano <robertsolorzano510@gmail.com>
2024-03-10 12:22:14 -0700

    updated correct path when importing from lib, finihsed constructing .then creating the svg with all user inputs and saves. need to fix text aligment issue as its not centered atm

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import inquirer from "inquirer";
-import { Circle, Square, Triangle } from './lib/shapes'
+import { Circle, Square, Triangle } from './lib/shapes.js'
+import { writeFileSync } from 'fs';
 
 // Promt for the user
-inquirer.prompt({
+inquirer.prompt([
     {
         type: 'list',
         name: 'shape',
         message: 'Choose a shape for your SVG logo.',
-        choises: ['Circle', 'Square', 'Triangle']
+        choices: ['Circle', 'Square', 'Triangle']
     },
     {
         type: 'input',
@@ -25,6 +26,30 @@ inquirer.prompt({
         name: 'textColor',
         message: 'Choose a color for your text. (Enter a color name or hex code)'
     }
-}).then(answers => {
+]).then(answers => {
+    const { shape, shapeColor, text, textColor } = answers;
+    let svgShape;
+
+    switch(shape) {
+        case 'Circle':
+            svgShape = new Circle(shapeColor);
+                break;
+        case 'Square':
+            svgShape = new Square(shapeColor);
+                break;
+        case 'Triangle':
+            svgShape = new Triangle(shapeColor);
+                break;
+        }
     
+    // Generate the SVG content combining the user-selected shape and text and colors
+    const svgContent = 
+    `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"> ${svgShape.render()}
+        
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}">${text}</text>
+    </svg>`;
+
+    // Save SVG to output folder
+    writeFileSync('./output/logo.svg', svgContent);
+    console.log('Generated logo.svg');
 })
